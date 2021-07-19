ed220f4202f7c86b94d93b298ae90782e79f4d46
github daisukenoguchi <daisukenoguemon@gmail.com>
2021-07-19 17:08:30 +0900

    add auth crud

--- handlers/auth.go
+++ handlers/auth.go
@@ -1,12 +1,19 @@
 package handlers
 
 import (
+	"github.com/gorilla/mux"
 	"github.com/noguchidaisuke/api-templates/util"
+	"github.com/noguchidaisuke/micro-service-with-mongo/api/restutil"
+	"io"
 	"net/http"
 )
 
 type AuthHandlers interface {
+	SignUp(w http.ResponseWriter, r *http.Request)
+	PutUser(w http.ResponseWriter, r *http.Request)
+	GetUser(w http.ResponseWriter, r *http.Request)
 	GetUsers(w http.ResponseWriter, r *http.Request)
+	DeleteUser(w http.ResponseWriter, r *http.Request)
 }
 
 type authHandlers struct {}
@@ -15,7 +22,37 @@ func NewAuthHandlers() AuthHandlers {
 	return &authHandlers{}
 }
 
+func (h *authHandlers) SignUp(w http.ResponseWriter, r *http.Request) {
+	if r.Body == nil {
+		restutil.WriteError(w, http.StatusBadRequest, restutil.ErrEmptyBody)
+		return
+	}
+
+	defer r.Body.Close()
+
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		restutil.WriteError(w, http.StatusBadRequest, err)
+		return
+	}
+
+	util.WriteAsJson(w, http.StatusOK, body)
+}
+func (h *authHandlers) PutUser(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	util.WriteAsJson(w, http.StatusOK, vars["id"])
+}
+
+func (h *authHandlers) GetUser(w http.ResponseWriter, r *http.Request) {
+	p := r.URL.Path
+	util.WriteAsJson(w, http.StatusOK, p)
+}
 func (h *authHandlers) GetUsers(w http.ResponseWriter, r *http.Request) {
+	p := r.URL.Path
+	util.WriteAsJson(w, http.StatusOK, p)
+}
+func (h *authHandlers) DeleteUser(w http.ResponseWriter, r *http.Request) {
+
 	p := r.URL.Path
 	util.WriteAsJson(w, http.StatusOK, p)
 }
--- routes/auth.go
+++ routes/auth.go
@@ -8,9 +8,30 @@ import (
 func NewAuthRoutes(authHandlers handlers.AuthHandlers) []*Route {
 	return []*Route{
 		{
+			Path: "/signup",
+			Method: http.MethodPost,
+			Handler: authHandlers.SignUp,
+		},
+		{
+			Path: "/users/{id}",
+			Method: http.MethodGet,
+			Handler: authHandlers.GetUser,
+		},
+		{
 			Path: "/users",
 			Method: http.MethodGet,
 			Handler: authHandlers.GetUsers,
+		},
+		{
+			Path: "/users/{id}",
+			Method: http.MethodPut,
+			Handler: authHandlers.PutUser,
+		},
+
+		{
+			Path: "/users/{id}",
+			Method: http.MethodDelete,
+			Handler: authHandlers.DeleteUser,
 		},
 	}
 }
